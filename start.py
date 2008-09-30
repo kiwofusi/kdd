fdb3afdcbe0767cd498004fb32d5a51ea4751cc4
kiwofusi <kiwofusi@gmail.com>
2008-09-30 23:47:21 +0900

    first commit

--- start.py
+++ start.py
@@ -3,6 +3,43 @@
 """
 from start import *
 で読み込み
+
+
+
+
+# テーブル設計
+CREATE TABLE entry(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  url VARCHAR(255) UNIQUE,
+  title VARCHAR(255)
+);
+
+CREATE TABLE tag(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) UNIQUE,
+  frequency INT
+);
+
+CREATE TABLE entry_tags(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  e_id INT,
+  t_id INT,
+  UNIQUE(e_id, t_id),
+  FOREIGN KEY(e_id) REFERENCES entry(id),
+  FOREIGN KEY(t_id) REFERENCES tag(id)
+);
+
+CREATE TABLE tags_sim(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  sim FLOAT,
+  t_id1 INT,
+  t_id2 INT,
+  weight INT,
+  UNIQUE(t_id1, t_id2),
+  FOREIGN KEY(t_id1) REFERENCES tag(id),
+  FOREIGN KEY(t_id2) REFERENCES tag(id)
+);
+
 """
 
 from hatebu_rec import *
